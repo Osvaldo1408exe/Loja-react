c83b5b24ba26bf67a63be3cd0baad6101ba89f1b
Osvaldo1408exe <osvaldo1408.exe@gmail.com>
2024-06-28 23:19:47 -0300

    Navbar com funcao hamburger

--- src/Pages/RouteLayout.jsx
+++ src/Pages/RouteLayout.jsx
@@ -2,26 +2,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, Outlet } from "react-router-dom";
 import { faShoppingCart, faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react';
 
  
 export default function RouteLayout(){
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+      setIsOpen(!isOpen);
+  }
     return (
         <>
-          <header>
-
-            <Link to="/" className="logo"><img src='https://github.com/Osvaldo1408exe/Loja-react/blob/main/public/icon.png?raw=true' style={{width: 50}}></img> </Link>
-            <nav className='navCenter'>
-              <Link to="/">Início</Link>
-              <Link to="/items">Items</Link>
-              <Link to="">Sobre</Link>
-              <Link to="">Contato</Link>
-
-            </nav>
-            <nav>
-              <Link  className="btn btn-outline-dark m-2"><FontAwesomeIcon icon={faSignInAlt} /> Entrar</Link>
-              <Link   className="btn btn-outline-dark m-2"><FontAwesomeIcon icon={faUserPlus} /> Registra</Link>
-              <Link  className="btn btn-outline-dark m-2"><FontAwesomeIcon icon={faShoppingCart} /> Carrinho </Link>       
-             </nav>
+          <header className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <Link to="/" className="navbar-brand">
+                    <img src='https://github.com/Osvaldo1408exe/Loja-react/blob/main/public/icon.png?raw=true' style={{ width: 50 }} alt="Logo" />
+                </Link>
+                <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`}>
+                    <nav className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <Link to="/" className="nav-link">Início</Link>
+                        <Link to="/items" className="nav-link">Items</Link>
+                        <Link to="/about" className="nav-link">Sobre</Link>
+                        <Link to="/contact" className="nav-link">Contato</Link>
+                    </nav>
+                    <div className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <Link to="/login" className="btn btn-outline-dark m-2"><FontAwesomeIcon icon={faSignInAlt} /> Entrar</Link>
+                        <Link to="/register" className="btn btn-outline-dark m-2"><FontAwesomeIcon icon={faUserPlus} /> Registrar</Link>
+                        <Link to="/cart" className="btn btn-outline-dark m-2"><FontAwesomeIcon icon={faShoppingCart} /> Carrinho</Link>
+                    </div>
+                </div>
+            </div>
           </header>
           <div>
             <Outlet />
@@ -40,3 +53,5 @@ export default function RouteLayout(){
         </>
       )
 }
+
+ 
